c7f648113a4bd65b1b3b7b8a6bf273617763ba19
Ricardo Persaud <ricardo39985@gmail.com>
2019-12-14 22:07:32 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -10,5 +10,5 @@ def happy_birthday(birthday_kids)
 end
 
 def age_appropriate_birthday(birthday_kids)
-  birthday_kids.each { |name, age| age >= 12 ? puts "Happy Birthday #{name}! You are now #{age} years old!" : puts "You are too old for this"}
+  birthday_kids.each {|name, age| age >= 12 ? puts "Happy Birthday #{name}! You are now #{age} years old!" : puts "You are too old for this"}
 end
